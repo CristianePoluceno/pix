547e66f93a3d992d206073ae3044ab2a79752518
CristianePoluceno <74864132+CristianePoluceno@users.noreply.github.com>
2021-09-28 21:11:30 -0300

    Criando usuario (#2)
    Co-authored-by: sti <sti@matera.com>

---
+++ src/main/java/matera/bootcamp/pix/domain/model/ContaCorrente.java
@@ -0,0 +1,28 @@
+package matera.bootcamp.pix.domain.model;
+
+import lombok.Data;
+import org.hibernate.id.CompositeNestedGeneratedValueGenerator;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+public class ContaCorrente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+//(nullable = false) = significa que não pode ser nulo
+    @Column(nullable = false)
+    private Long agencia;
+
+    @Column(nullable = false)
+    private Long conta;
+
+    @Column(precision = 10,scale=2, nullable = false)
+    private BigDecimal saldo;
+
+}
