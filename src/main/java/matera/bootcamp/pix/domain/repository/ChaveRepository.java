2986660a78d83cc7ceccec48f339db1e20dd85e9
sti <sti@matera.com>
2021-09-30 21:05:08 -0300

    Criacao de api de cadastro de chave

---
+++ src/main/java/matera/bootcamp/pix/domain/repository/ChaveRepository.java
@@ -0,0 +1,6 @@
+package matera.bootcamp.pix.domain.repository;
+import matera.bootcamp.pix.domain.model.Chave;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChaveRepository  extends JpaRepository<Chave, Long> {
+}
